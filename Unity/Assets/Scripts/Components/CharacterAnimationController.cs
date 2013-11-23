4ae6764b61d196ba62bc9420e17cd19a52646cc9
temps <rasmus.hook@gmail.com>
2013-11-23 23:33:28 +0100

    dummy state and stuff

--- Unity/Assets/Scripts/Components/CharacterAnimationController.cs
+++ Unity/Assets/Scripts/Components/CharacterAnimationController.cs
@@ -8,8 +8,13 @@ public class CharacterAnimationController : MonoBehaviour
 	Animator _Animator;
 	Transform _Transform;
 
-	[NonSerialized]
-	public Vector3 Velocity;
+	public DummyPlayer LogicCharacter;
+
+	private Vector3 Velocity;
+
+	private DummyTimeline _StateTimeline;
+	private AnimatorStateInfo _CurrentStateInfo;
+	private AnimatorStateInfo _NextStateInfo;
 
 	void Awake () 
 	{
@@ -19,6 +24,27 @@ public class CharacterAnimationController : MonoBehaviour
 
 	void Update () 
 	{
+		if (LogicCharacter != null)
+		{
+			_StateTimeline = LogicCharacter.GetStateTimeline();
+			switch (_StateTimeline.DummyState)
+			{
+			case DummyState.Idle: 
+				DoIdle();
+				break;
+			case DummyState.Mining:
+				DoMining();
+				break;
+			case DummyState.Movement:
+				DoMovement();
+				break;
+			}
+		}
+
+		_CurrentStateInfo = _Animator.GetCurrentAnimatorStateInfo(0);
+		_NextStateInfo = _Animator.GetNextAnimatorStateInfo(0);
+
+		/*
 		if (Velocity != Vector3.zero)
 			_Transform.rotation = Quaternion.LookRotation(Velocity.normalized);
 
@@ -26,6 +52,7 @@ public class CharacterAnimationController : MonoBehaviour
 		float sizeScaleFactor = _Transform.localScale.x * _Animator.humanScale;
 		float velocityMagnitude = Velocity.magnitude / sizeScaleFactor;
 		_Animator.SetFloat("Speed", velocityMagnitude);
+		*/
 	}
 
 	public void PlayAnimation(AnimationEnum animationEnum)
@@ -33,4 +60,33 @@ public class CharacterAnimationController : MonoBehaviour
 		_Animator.Play(Animations.GetAnimationHash(animationEnum));
 		//Debug.Log("Play animation: " + animation.ToString() + "  " + Time.time);
 	}
+
+	void DoIdle()
+	{
+		
+	}
+
+	void DoMining()
+	{
+		if (_CurrentStateInfo.nameHash != Animations.GetAnimationHash(AnimationEnum.Laugh) ||
+		    _NextStateInfo.nameHash != Animations.GetAnimationHash(AnimationEnum.Laugh))
+			PlayAnimation(AnimationEnum.Laugh);
+	}
+
+	void DoMovement()
+	{
+		Debug.Log(_StateTimeline);
+		float lerp = (Time.time - _StateTimeline.StartTime) / (_StateTimeline.EndTime - _StateTimeline.StartTime);
+		Vector3 position = Vector3.Lerp(_StateTimeline.StartPos, _StateTimeline.EndPos, lerp);
+		_Transform.position = position;
+	}
 }
+
+
+
+
+
+
+
+
+
--- Unity/Assets/Scripts/Components/Dummy/DummyPlayer.cs
+++ Unity/Assets/Scripts/Components/Dummy/DummyPlayer.cs
@@ -1,71 +1,102 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-[RequireComponent(typeof(CharacterAnimationController))]
+public enum DummyState
+{
+	Idle,
+	Movement,
+	Mining
+}
+
+public class DummyTimeline
+{
+	public DummyState DummyState;
+	public float StartTime;
+	public float EndTime;
+	public Vector3 StartPos;
+	public Vector3 EndPos;
+}
+
 public class DummyPlayer : MonoBehaviour 
 {
 	float _ActionTimer;
 	Vector3 _TargetPos;
 
 	public float MoveSpeed = 10;
-	private CharacterAnimationController _Controller;
 	private Vector3 _LastPosition;
 
 	public float CircleSpeed = 1;
 	public float CircleSize = 5;
 
-	public bool RandomActions;
+	public bool UseTimeline;
 	private bool _ReachedTarget;
 	private bool _PlayedAnimation;
 
+	public DummyState DummyState;
+
+	DummyTimeline _Timeline;
+
 	void Awake()
 	{
-		_Controller = GetComponent<CharacterAnimationController>();
 		_LastPosition = transform.position;
+		_Timeline = new DummyTimeline();
+		SetTimeline(DummyState.Idle);
+	}
+
+	void Start()
+	{		
+		CharacterAnimationController controller = GetComponent<CharacterAnimationController>();
+		controller.LogicCharacter = this;
+	}
+
+	public DummyTimeline GetStateTimeline()
+	{
+		return _Timeline;
+	}
+
+	void SetTimeline(DummyState state)
+	{
+		_Timeline.DummyState = state;
+		_Timeline.StartTime = Time.time;
+
+		if (state == DummyState.Movement)
+		{
+			_Timeline.StartPos = transform.position;
+			_Timeline.EndPos = new Vector3(Random.Range(-10f, 10f), 0, Random.Range(-10f, 10f));
+			_Timeline.EndTime = Time.time + Vector3.Distance(_Timeline.StartPos, _Timeline.EndPos) * MoveSpeed;
+		}
+		if (state == DummyState.Idle)
+		{
+			_Timeline.EndTime = Time.time + 5f;
+		}
+		if (state == DummyState.Mining)
+		{
+			_Timeline.EndTime = Time.time + 3f;
+		}
+	}
+
+	DummyState GetRandomState()
+	{
+		int random = Random.Range(0, 3);
+		if (random == 0)
+			return DummyState.Idle;
+		else if (random == 1)
+			return DummyState.Movement;
+		else 
+			return DummyState.Mining;
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		if (RandomActions)
+		if (UseTimeline)
 		{
-			_ActionTimer -= Time.deltaTime;
-			if (_ActionTimer < 0)
-			{
-				_TargetPos = new Vector3(Random.Range(-10f, 10f), 0, Random.Range(-10f, 10f));
-				_ReachedTarget = false;
-				_PlayedAnimation = false;
-				_ActionTimer = 15f;
-			}
-			if (_ReachedTarget == false)
-			{	
-				Vector3 newPosition = Vector3.MoveTowards(transform.position, _TargetPos, Time.deltaTime * MoveSpeed);
-				if (newPosition == _LastPosition)
-					_ReachedTarget = true;
-				_Controller.Velocity = (newPosition - _LastPosition) / Time.deltaTime;
-				_LastPosition = newPosition;
-				transform.position = newPosition;
-			}
-			else
-			{
-				if (_PlayedAnimation == false)
-				{					
-					AnimationEnum animEnum = AnimationEnum.Laugh;
-					int anim = Random.Range(0, 3);
-					if (anim == 0)
-						animEnum = AnimationEnum.Enraged;
-					if (anim == 1)
-						animEnum = AnimationEnum.Laugh;
-					if (anim == 2)
-						animEnum = AnimationEnum.AttackSlash;
+			// New timeline
+			if (Time.time > _Timeline.EndTime)
+				SetTimeline(GetRandomState());
 
-					_Controller.PlayAnimation(animEnum);
-					
-					_ActionTimer = Animations.GetAnimationData(animEnum).AnimationLenght;
-					_PlayedAnimation = true;
-				}
-			}
-
+			DummyState = _Timeline.DummyState;
 		}
 		else
 		{
@@ -75,9 +106,6 @@ public class DummyPlayer : MonoBehaviour
 			_TargetPos = new Vector3(x, 0, z);
 			_LastPosition = transform.position;
 			transform.position = _TargetPos;
-			_Controller.Velocity = (_TargetPos - _LastPosition) / Time.deltaTime;
-
 		}
-
 	}
 }
